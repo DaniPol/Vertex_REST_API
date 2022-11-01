2afd96d3606034107f1450c7f27b9401cb24ce94
Daniel Polevikov <danielpol169@gmail.com>
2022-11-01 22:23:55 +0200

    pom.xml updated for hazelcast, main.java has commented code for hazelcast cluster

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 
 
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
 import org.example.verticles.OrderVerticle;
 import org.example.verticles.RestVerticle;
 
@@ -15,5 +20,32 @@ public class Main {
             if (ar1.succeeded())
                 vertx.deployVerticle(RestVerticle.class, new DeploymentOptions());
         });
+
+
+//        ClusterManager mgr = new HazelcastClusterManager();
+//        VertxOptions options = new VertxOptions().setClusterManager(mgr);
+//        Vertx.clusteredVertx(options,res->{
+//            if(res.succeeded()){
+//                System.out.println("Cluster created");
+//                Vertx vertx = res.result();
+//                vertx.deployVerticle(new OrderVerticle(),res1->{
+//                    if (res1.succeeded()) {
+//                        System.out.println("deployVerticle deployed");
+//                        vertx.deployVerticle(new RestVerticle(), res2 -> {
+//                            if (res2.succeeded())
+//                                System.out.println("RestVerticle deployed");
+//                            else
+//                                System.out.println("RestVerticle not deployed");
+//                        });
+//                    }
+//                    else
+//                        System.out.println("deployVerticle not deployed");
+//                });
+//            }
+//            else
+//                System.out.println("Cluster not created");
+//
+//        });
+
     }
 }
